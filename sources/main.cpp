924fb01a7aba53cc0cafd7ab59539f6056500a8e
Murderdoll <artem765@inbox.ru>
2017-10-02 00:18:46 +0300

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,36 +1,34 @@
-#include <iostream>
 #include "Stack.hpp"
-using namespace std;
 
-int main()
-{
-	try
-	{
-	Stack<int> Ob(3);
-	Ob.push(1);
-	Ob.push(2);
-	Ob.push(3);
-	Ob.push(4);
-	for (int i = 0; i < 4; i++)
-	{
-		Ob.showMe(i);// заполняем наш стек и выодим его содержимое на экран
-	}
-	cout <<   endl;
-	Ob.pop(); // убираем верхний элемент из стека
-	for (int i = 0; i < 4; i++)
-	{
-		Ob.showMe(i);
-	}
+void stackInit() {
+	cout << endl << "---init.cpp---" << endl;
+	Stack<int> arrayInt;
+	cout << "arrayInt created" << endl;
+}
+
+void stackPush() {
+	cout << endl << "---push.cpp---" << endl;
+	Stack<int> arrayInt;
+	for(int i = 0; i < 10; i++) { arrayInt.push(i*2); }
+	for(size_t j = 0; j < arrayInt.count(); j++) { cout << arrayInt[j] << " "; }
 	cout << endl;
-	Ob.pop();
-	Ob.pop();
-	Ob.pop();
-	cout << "Now Stack is empty." << endl;
-	//Ob.pop(); // Вызываем аварийную остановку программы, выходя за границы стека
-        }
-	catch (const exception& e)  	//обработка исключений
-	{
-		cout << e.what() << endl;
+}
+
+void stackPop() {
+	cout << endl << "---pop.cpp---" << endl;
+	Stack<int> arrayInt;
+	for(int i = 0; i < 10; i++) { arrayInt.push(i); }
+	for(int j = 0; j < 6; j++) { cout << arrayInt.pop() << " "; }
+	cout << endl << endl;
+}
+
+void stackEmpty() {
+	cout << endl << "---empty.cpp---" << endl;
+	Stack<char> arrayChar;
+	char wordChar[] = "KCATS_RAHC";
+	for(int i = 0; i < strlen(wordChar); i++) { arrayChar.push(wordChar[i]); }
+	while(!arrayChar.empty()) {
+		cout << arrayChar.pop() << " ";
 	}
-	return 0;
+	cout << endl << endl;
 }
